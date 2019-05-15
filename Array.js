ad9fe2f70d794c02446de324fedf825570f4bdff
Enrique Montemayor <eemontemayor@live.com>
2019-05-15 13:49:41 -0500

    finished 1-4

--- Array.js
+++ Array.js
@@ -80,8 +80,47 @@ function main(){
 
   // Add an item to the array
   arr.push(3);
+  // What is the length, capacity and memory address of your array?
+  // length :1, capacity:3, ptr:0
+  //
 
-  console.log(arr);
+
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+  //What is the length, capacity and memory address of your array? 
+  //Explain the result of your program after adding the new lines of code.
+  //Array { length: 6, ptr: 3, _capacity: 12 }
+  // The length is six because there are six items pushed into the array
+  // The pointer is 3 because it was set there when we had resize after the 4th call to push into array
+  // The capacity is 12 because when the array was resized it added +1 to length and multiplied it by 3
+  
+  
+  
+  arr.pop();
+  arr.pop();
+  arr.pop();
+  arr.pop();
+  arr.pop();
+  arr.pop();
+ // What is the length, capacity, and address of your array? Explain the result of your program after adding the new lines of code.
+  //Array { length: 3, ptr: 3, _capacity: 12 }
+  // The length is three because pop removes the last of the array and it was invoked 3 times
+  // The pointer is 3 because it only gets adjusted when the array is resized and this doesn't occur with pop method
+  // The capacity is 12 because it only gets adjusted when the array is resized and this doesn't occur with pop method
+
+  arr.push("tauhida");
+  //Print this 1 item that you just added. What is the result? Can you explain your result?
+  // Result: NaN
+  // Memory is implemented in a manner that only accepts floating numbers and, so, cannot read a string
+  
+
+//What is the purpose of the _resize() function in your Array class?
+  // It makes the memory storage bigger if the Array we are trying to insert does not fit in its current allocation
+
+  console.log(arr.get(0));
 }
 main()
 module.exports = Array;
